1ee268fce6891212d7b176630a2301c8dee46931
Takayuki NAGATOMI <tommyt6073@gmail.com>
2020-02-07 08:26:48 +0900

    悲劇を「ポリモーフィズムによる条件記述  の置き換え

--- PerformanceCalculator.php
+++ PerformanceCalculator.php
@@ -17,11 +17,7 @@ class PerformanceCalculator
         $result = 0;
         switch ($this->play['type']) {
             case 'tragedy':
-                $result = 40000;
-                if ($this->performance['audience'] > 30) {
-                    $result += 1000 * ($this->performance['audience'] - 30);
-                }
-                break;
+                throw new Error('想定外の呼び出し');
             case 'comedy':
                 $result = 30000;
                 if ($this->performance['audience'] > 20) {
--- TragedyCalculator.php
+++ TragedyCalculator.php
@@ -3,5 +3,12 @@
 
 class TragedyCalculator extends PerformanceCalculator
 {
-
+    public function amount()
+    {
+        $result = 40000;
+        if ($this->performance['audience'] > 30) {
+            $result += 1000 * ($this->performance['audience'] - 30);
+        }
+        return $result;
+    }
 }
